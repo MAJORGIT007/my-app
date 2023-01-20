0fedf68ea03f1354fbabcdc9ac8fecc2ab21013f
MAJOR <jklee007@mymail.sim.edu.sg>
2023-01-20 18:03:42 +0800

    update

---
+++ supabaseClient.js
@@ -0,0 +1,3 @@
+  import { createClient } from '@supabase/supabase-js'
+
+  export const supabase = createClient('https://dbtnqrzkhafpkicnnxlj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRidG5xcnpraGFmcGtpY25ueGxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQyMDg0NjUsImV4cCI6MTk4OTc4NDQ2NX0.cnKolbAmDkVFgJWmzD8YxeZctAv7zP3ytf-IsUf22z0')
